ce5f0fb37d505966755717d1d7831c0c55df5493
sandzan <sandra@carlsson4.se>
2025-03-02 17:00:37 +0100

    Smal changes

--- frontend/src/controllers/axiosTemplate.js
+++ frontend/src/controllers/axiosTemplate.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 export const api = axios.create({
-    baseURL: "http://9.163.80.137:80/",
+    baseURL: "http://backend:80/",
     withCredentials: true,
     headers:{
       "Access-Control-Allow-Credentials":"true",
