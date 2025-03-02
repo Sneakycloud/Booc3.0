6c6c2722e15bc929ec2eb8873b3d75a2ae5c940f
sandzan <sandra@carlsson4.se>
2025-03-02 21:16:45 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ const corsconfig = {
 const server = http.createServer(app);
 io = new Server(server,{
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://132.164.30.109",
     credentials: true,
   }
 });
