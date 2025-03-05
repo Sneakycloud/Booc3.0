ffa6359d87b806fbce36460a279cb4518f2a9e3f
Eddie Olofsgård <eddieolofsgard@icloud.com>
2025-03-05 15:53:12 +0100

    logs for axios template

--- backend/AxiosTemplate/AxiosUserMs.js
+++ backend/AxiosTemplate/AxiosUserMs.js
@@ -4,9 +4,11 @@ const axios = require('axios');
 const usersMsApi = () => {
   var AxiosApi;
   if(process.env?.PROD == true || process.env?.PROD == "true"){
+    console.log("BaseURL called with azure");
     AxiosApi = axios.create({baseURL:`http://users-microservice`});
   }
   else{
+    console.log("BaseURL called with localhost");
     AxiosApi = axios.create({baseURL:"http://localhost:4000/"});
   }
   return AxiosApi;
