337ecc692eaaab49177490068f0a5455269886d2
sandzan <sandra@carlsson4.se>
2025-03-15 14:51:42 +0100

    Update groupController.js
    Pls work

--- backend/Controller/groupController.js
+++ backend/Controller/groupController.js
@@ -62,7 +62,7 @@ async function createGroup(req, res){
             console.log("There was an attempt to create a group with an empty name");
             return res.status(500).send({msg:"Failed to create group"});
         }
-        const owner = [req.jwt.user];//{username:req.jwt.username, identifier:req.jwt.identifier}
+        const owner = [{username:req.jwt.payload.username, identifier:req.jwt.payload.identifier}];//{username:req.jwt.username, identifier:req.jwt.identifier}
         console.log("Kollar owner i backend------------------------:", owner);
         const memberObjectArray = members.map(inviteToObject);
 
